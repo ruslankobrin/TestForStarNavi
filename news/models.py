90383996b86956b7961f83c0b135f476d9ae7ab9
ruslankobrin <ruslankobrin@gmail.com>
2020-06-10 16:30:38 +0300

    Add Like and Unlike models.

--- news/models.py
+++ news/models.py
@@ -7,3 +7,21 @@ class Post(models.Model):
     text = models.CharField(max_length=800)
     creation_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    creation_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ['user', 'post']
+
+
+class Unlike(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    creation_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ['user', 'post']
