e49fedfb6d8c41304aa8b68f594bd3f9b70fe2cb
ruslankobrin <ruslankobrin@gmail.com>
2020-06-10 16:43:30 +0300

    Add Like and Unlike serialisers.

--- news/serializers.py
+++ news/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from news.models import Post
+from news.models import Post, Like, Unlike
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -8,3 +8,17 @@ class PostSerializer(serializers.ModelSerializer):
         model = Post
         fields = ['id', 'title', 'text', 'creation_date', 'author']
         read_only_fields = ['author']
+
+
+class LikeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Like
+        fields = ['user', 'post']
+        read_only_fields = ['user']
+
+
+class UnlikeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Unlike
+        fields = ['user', 'post']
+        read_only_fields = ['user']
